cf30f318d4f9d97475267e019feda36ab1c54b2e
Dylan Wolff <dylanwolff@gmail.com>
2015-07-19 21:03:36 +1000

    Build posts controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,19 +1,48 @@
 class Admin::PostsController < Admin::ApplicationController
   def new
+    @page_title = "Add Post"
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Post created"
+      redirect_to admin_posts_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @post = Post.find_by(params[:id])
   end
 
   def update
+    @post = Post.find_by(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to admin_posts_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(params[:id])
+    @post.destroy
+    flash[:danger] = "Post deleted"
+    redirect_to admin_posts_path
   end
 
   def index
+    @posts = Post.all
   end
 
-  def show
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :category_id, :user_id, :tags, :image,
+                                 :body)
   end
 end
