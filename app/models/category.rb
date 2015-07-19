eaffbce0d6b56aa94c25fdbe7fe2f166d461be76
Dylan Wolff <dylanwolff@gmail.com>
2015-07-19 20:07:58 +1000

    Add validations to category, post and user models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :posts
 
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
+
+  validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  validates_confirmation_of :password
 end
