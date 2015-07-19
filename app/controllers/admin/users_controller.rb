796a232d1a4932ddbc99acf7462e02dc7d863f04
Dylan Wolff <dylanwolff@gmail.com>
2015-07-19 21:03:28 +1000

    Build users controller and add authentication

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,48 @@
 class Admin::UsersController < Admin::ApplicationController
   def new
+    @page_title = "Add User"
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "User created"
+      redirect_to admin_users_path
+    else
+      render "new"
+    end
   end
 
   def edit
+    @user = User.find_by(params[:id])
   end
 
   def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "User updated"
+      redirect_to admin_users_path
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @user = User.find_by(params[:id])
+    @user.destroy
+    flash[:danger] = "User deleted"
+    redirect_to admin_users_path
   end
 
   def index
+    @users = User.all
   end
 
-  def show
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+
+  before_save :encrypt_password
+
   has_many :posts
 
-  validates :username, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: true
   validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password, confirmation: true
 
-  validates_confirmation_of :password
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
