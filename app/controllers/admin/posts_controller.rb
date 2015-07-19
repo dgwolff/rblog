6d2818eb71c60790e7b24d9d53ed6cfe20164067
Dylan Wolff <dylanwolff@gmail.com>
2015-07-19 14:33:01 +1000

    Add controllers, models and routes

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,19 @@
+class Admin::PostsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
