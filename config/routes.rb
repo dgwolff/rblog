38ff809a61cda03b31310bf22da3c23fecc86552
Dylan Wolff <dylanwolff@gmail.com>
2015-08-05 20:10:00 +1000

    Begin building out front end

--- config/routes.rb
+++ config/routes.rb
@@ -7,104 +7,16 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#index'
 
-  namespace :admin do
-    get 'sessions/new'
-  end
-
-  namespace :admin do
-    get 'sessions/create'
-  end
-
-  namespace :admin do
-    get 'users/new'
-  end
-
-  namespace :admin do
-    get 'users/create'
-  end
-
-  namespace :admin do
-    get 'users/edit'
-  end
-
-  namespace :admin do
-    get 'users/update'
-  end
-
-  namespace :admin do
-    get 'users/destroy'
-  end
-
-  namespace :admin do
-    get 'users/index'
-  end
-
-  namespace :admin do
-    get 'users/show'
-  end
-
-  namespace :admin do
-    get 'comments/destroy'
-  end
-
-  get 'comments/new'
-
-  namespace :admin do
-    get 'categories/new'
-  end
-
-  namespace :admin do
-    get 'categories/create'
-  end
-
-  namespace :admin do
-    get 'categories/edit'
-  end
-
-  namespace :admin do
-    get 'categories/update'
-  end
-
-  namespace :admin do
-    get 'categories/destroy'
-  end
-
-  namespace :admin do
-    get 'categories/index'
-  end
-
-  namespace :admin do
-    get 'posts/new'
-  end
-
-  namespace :admin do
-    get 'posts/create'
-  end
-
-  namespace :admin do
-    get 'posts/edit'
-  end
-
-  namespace :admin do
-    get 'posts/update'
-  end
-
-  namespace :admin do
-    get 'posts/index'
-  end
-
-  namespace :admin do
-    get 'posts/show'
-  end
-
-  get 'posts/index'
-
-  get 'posts/show'
-
-  resources :posts, :categories, :comments
   resources 'contacts', only: [:new, :create]
 
   namespace :admin do
-    resources :posts, :categories, :comments, :users
+    resources :posts, :categories, :comments, :users, :sessions
   end
+
+  resources :categories
+
+  resources :posts do
+    resources :comments
+  end
+
 end
