9b3b87606a4f5a6a65bdae131cff4e6bbbd58489
Dylan Wolff <dylanwolff@gmail.com>
2015-08-07 21:22:50 +1000

    Build out login/logout

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,7 +1,24 @@
 class Admin::SessionsController < Admin::ApplicationController
+  layout "admin/login"
+
   def new
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome, #{user.name} you're now logged in"
+      redirect_to admin_posts_path
+    else
+      flash[:danger] = "Invalid username or password"
+      redirect_to admin_login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "You're now logged out"
+    redirect_to admin_login_path
   end
 end
