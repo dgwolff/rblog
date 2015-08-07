dede70930a9ba21dbadc5b09573db82a7b2950f9
Dylan Wolff <dylanwolff@gmail.com>
2015-08-07 21:21:08 +1000

    Add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  get 'contact' => 'contacts#new'
-
-  get 'contacts/create'
-
   get 'about' => 'about#index'
+  get 'contact' => 'contacts#new'
+  get 'contacts/create'
 
   resources 'contacts', only: [:new, :create]
 
   namespace :admin do
+    get "login" => "sessions#new", as: "login"
+    get "logout" => "sessions#destroy", as: "logout"
     resources :posts, :categories, :comments, :users, :sessions
   end
 
@@ -18,5 +18,4 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-
 end
